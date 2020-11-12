17c849ebf96ffddf680a42b5b9f09f883f6b391d
Fredrik <fredrsan@gmail.com>
2020-11-12 12:15:39 +0100

    started frontend

---
+++ src/main/resources/static/js/confirmationPopUp.js
@@ -0,0 +1,11 @@
+let button_node = document.getElementById("test");
+
+let pop_up_node = document.createElement("div");
+pop_up_node.classList.add("pop-up-window");
+
+let warning_text_node = document.createElement("p");
+warning_text_node.innerText = "Once it is deleted it is lost forever";
+warning_text_node.style.color = "red";
+
+let confirmation_form_node = document.createElement("form");
+
--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -1,15 +1,20 @@
+let headerNode = document.querySelector("header");
 
-window.onscroll = function() {myFunction()};
+let scroll_position;
+let off_set_value = headerNode.clientHeight;
 
-var headerNode = document.getElementById("header");
-var sticky = header.offsetTop;
 
-    
+function offset(){ off_set_value = headerNode.clientHeight};
 
-function myFunction() {
-    if (window.pageYOffset > sticky) {
-	header.classList.add("sticky");
+let check_header = _.throttle(()=>{
+    scroll_position = Math.round(window.scrollY);
+    if (scroll_position > off_set_value){
+	headerNode.classList.add("sticky");
     } else {
-	header.classList.remove("sticky");
+	headerNode.classList.remove("sticky");
     }
-}
+    
+}, 300);
+
+window.addEventListener("resize", offset)
+window.addEventListener("scroll", check_header);
